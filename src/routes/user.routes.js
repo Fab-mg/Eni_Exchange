9c08df499b32cfeb01c740f5d88007124dce2c67
Fabio <lucfabior@gmail.com>
2023-09-20 00:46:46 +0300

    auth guard

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/user.controller');
+const { authenticateToken } = require('../middlewares/auth.guard');
 
 router.route('/register').post(usersController.createNewUser);
 
